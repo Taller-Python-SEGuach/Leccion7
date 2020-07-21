da3d70b0816df518fe9768707f7b0a29f8213952
GonzaloMGuia <53207908+GonzaloMGuia@users.noreply.github.com>
2020-07-21 17:16:48 -0400

    Update computepayGM.py

--- computepayGM.py
+++ computepayGM.py
@@ -1,13 +1,13 @@
-def computepay(x):
+def computepay(x,z):
     if x<=40:
-        y= x*10.5
+        y= x*z
     else:
-        y=(x-40)*15.75+x*10.5
+        y=(x-40)*z*1.5+x*z
     return y
 
 print('Calcularemos cuanto dinero debe recibir  ')
 
 a= float(input('Ingresa numero de horas trabajadas a la semana'))
+b= float(input('Ingresa valor de tu hora de trabajo'))
 
-
-print(computepay(a))
+print(computepay(a,b))

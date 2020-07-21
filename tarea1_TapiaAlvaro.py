5899eb784f2d97261450af405d41639dac5b905f
alvarotbarrueto <68161198+alvarotbarrueto@users.noreply.github.com>
2020-07-21 17:18:13 -0400

    Add files via upload

---
+++ tarea1_TapiaAlvaro.py
@@ -0,0 +1,17 @@
+def computepay(h,p):
+    if h<=40:
+        pay=h*p
+        return pay
+    elif h>40:
+        pay=40*p + (h-40)*p*1.5
+        return pay
+
+h=input("¿Cuantas horas trabajas?")
+p=input("¿Cuanto te pagan por hora?")
+try :
+   h = float(h)
+   p = float(p)
+   computepay(h,p)
+   print("Te pagarán", computepay(h,p))
+except :
+    print("no ingreso un numero entero")

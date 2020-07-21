275ba3935f8078b116ed3e886e695848db0086cd
SebaRivasCL <68251231+SebaRivasCL@users.noreply.github.com>
2020-07-21 17:29:27 -0400

    Update tarea_rivas.py

--- tarea_rivas.py
+++ tarea_rivas.py
@@ -4,6 +4,6 @@ def computepay(horas):
     if horas > 40:
         print((horas-minimo)*1.5*xhora + minimo*xhora)
     else:
-        print(horas*10.50)
+        print(horas*xhora)
 input = input("cuantas horas trabajo :")
 computepay(float(input))

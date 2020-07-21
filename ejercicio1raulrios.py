af4e1dddaca730b045be9ba42c7abdc4b48a5b3d
Raulencio <47399093+Raulencio@users.noreply.github.com>
2020-07-21 17:27:58 -0400

    Update ejercicio1raulrios.py

--- ejercicio1raulrios.py
+++ ejercicio1raulrios.py
@@ -1,3 +1,8 @@
+#x resultado final
+#h horas ingresadas
+#p valor por hora ingresada
+#c cantidad de horas extras
+
 def computepay(h,p):
     x=0.0
     if(h>40):
@@ -6,9 +11,12 @@ def computepay(h,p):
     elif(h<=40):
         x=h*p
     return x
+
 #ht horas trabajadas
 #ph precio por hora
-
-ht=float(input("Cantidad de horas trabajadas "))
-ph=float(input("Valor por hora "))
-print("Valor total",computepay(ht,ph),"por",ht,"horas trabajadas")
+try:
+    ht=float(input("Cantidad de horas trabajadas "))
+    ph=float(input("Valor por hora "))
+    print("Valor total",computepay(ht,ph),"por",ht,"horas trabajadas")
+except:        
+    print("Error")

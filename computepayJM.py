cb9925d47d65494a6746c11e0afe7a22a7a33d1e
JavierJam <68250060+JavierJam@users.noreply.github.com>
2020-07-21 19:32:54 -0400

    Update computepayJM.py

--- computepayJM.py
+++ computepayJM.py
@@ -2,7 +2,7 @@ a =float(input("ingrese la cantidad de horas:"))
 b =float(input("ingrese la paga por hora: "))
 def computepay(a):
     if a <= 40:
-        c = a * 10
+        c = a * b
     else:
         c= ((a - 40)* b * 1.5) + 40 * b
     return c
